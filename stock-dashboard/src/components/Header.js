5c0d952d714c14a5b554626a7ca46eceb06bf518
roeeb <roeebina5@gmail.com>
2024-07-19 19:58:01 +0300

    tradingview front

--- stock-dashboard/src/components/Header.js
+++ stock-dashboard/src/components/Header.js
@@ -1,4 +1,3 @@
-// src/components/Header.js
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
@@ -39,12 +38,20 @@ const Header = () => {
 
     return (
         <>
+            <iframe 
+                scrolling="no" 
+                allowtransparency="true" 
+                frameborder="0" 
+                src="https://www.tradingview-widget.com/embed-widget/ticker-tape/?locale=en#%7B%22symbols%22%3A%5B%7B%22description%22%3A%22%22%2C%22proName%22%3A%22NASDAQ%3ATSLA%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22NASDAQ%3ANVDA%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22NASDAQ%3AAAPL%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22NASDAQ%3AAMD%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22NASDAQ%3AAMZN%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22NASDAQ%3AMSFT%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22NASDAQ%3ANFLX%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22NASDAQ%3AMETA%22%7D%2C%7B%22description%22%3A%22%22%2C%22proName%22%3A%22NASDAQ%3AINTC%22%7D%5D%2C%22showSymbolLogo%22%3Atrue%2C%22isTransparent%22%3Afalse%2C%22displayMode%22%3A%22adaptive%22%2C%22colorTheme%22%3A%22dark%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A78%2C%22utm_source%22%3A%22index-tracker.netlify.app%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22ticker-tape%22%2C%22page-uri%22%3A%22index-tracker.netlify.app%2F%22%7D" 
+                title="ticker tape TradingView widget" 
+                lang="en" 
+                style={{ userSelect: 'none', boxSizing: 'border-box', display: 'block', height: '78px', width: '100%' }}>
+            </iframe>
             <header className="bg-dark-card border-b border-gray-700">
                 <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <nav className="flex items-center justify-between h-16 lg:h-20">
                         <div className="flex items-center space-x-4">
                             <Link to="/" className="text-2xl font-bold text-primary">Stock Dashboard</Link>
-                            {/* Main Navigation Links */}
                             <div className={`md:flex md:items-center md:space-x-10 ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
                                 <Link to="/articles" className="text-sm font-medium text-dark-text transition-all duration-200 lg:text-base hover:text-primary focus:text-primary"> Articles </Link>
                                 <Link to="/tutorials" className="text-sm font-medium text-dark-text transition-all duration-200 lg:text-base hover:text-primary focus:text-primary"> Tutorials </Link>
@@ -53,7 +60,6 @@ const Header = () => {
                             </div>
                         </div>
                         <div className="flex items-center space-x-4">
-                            {/* Search Bar and Mobile Menu Button */}
                             <SearchBar setStockData={handleSetStockData} />
                             <button 
                                 type="button" 
